09f3f0cd9016147e9dea426b4de12c53839d9a69
staceyxyzlee <staceylee650@gmail.com>
2025-06-30 21:16:54 -0700

    mobile nav + width
    import nav, fix vertical widths

---
+++ js/navbar.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function(){
+    fetch('navbar.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("error in response");
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById("nav-placeholder").innerHTML = data;
+            const currentURL = window.location.href;
+            const links = document.querySelectorAll("a");
+            links.forEach(function(link){
+                if (link.href === currentURL){
+                    link.classList.add("active");
+                }
+            })
+        })
+        .catch(error => {
+            console.error("problem fetching navbar");
+        })
+})

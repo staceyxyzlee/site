cae1e12f01409f92c564dc3111a473cd5870560c
staceyxyzlee <staceylee650@gmail.com>
2026-03-24 14:30:31 -0700

    redirect pages

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
-    fetch('navbar.html')
+    fetch('/navbar.html')
         .then(response => {
             if (!response.ok) {
                 throw new Error("error in response");
--- run.sh
+++ run.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 cd "$(dirname "$0")"
-uv run python -m http.server
+uv run python -c "
+import http.server, os
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        path = self.translate_path(self.path)
+        if not os.path.exists(path) and not self.path.endswith('/'):
+            if os.path.exists(path + '.html'):
+                self.path += '.html'
+        super().do_GET()
+
+print('Serving at http://localhost:8000')
+http.server.HTTPServer(('localhost', 8000), Handler).serve_forever()
+"
